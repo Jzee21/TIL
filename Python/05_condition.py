52f8b0c95ada74e543bdb55550549c59f377583c
Jzee21 <jzee21b@gmail.com>
2019-12-18 15:37:34 +0900

    Comdition_if, elif, else

---
+++ Python/05_condition.py
@@ -0,0 +1,16 @@
+# 05_condition.py
+
+dust = 120
+
+print(dust>150)
+print(dust>100)
+
+# if True :
+#     print("너무 드러워")
+
+if dust > 150 :
+    print("So Bad")
+elif dust > 100 :
+    print("Bad")
+else:
+    print("Good")
