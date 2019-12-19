5cfd311dac360c9b6c2a5bfe55d8b1cf167c1f4d
Jzee21 <jzee21b@gmail.com>
2019-12-19 17:14:43 +0900

    Crawling using with open file

--- Python/Crawling/exchange.py
+++ Python/Crawling/exchange.py
@@ -17,4 +17,3 @@ print(f"미국USD 환율 : {result.text}")
 # with : 스코프.    # heavy, high resorce.
 with open('exchange_text.txt', 'w', encoding='utf-8') as f :
     f.write(f"현재 원/달러 환율 : {result.text}")
-
