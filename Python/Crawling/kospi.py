70412e655f5a670faa614f5640b918dbbba7a142
Jzee21 <jzee21b@gmail.com>
2019-12-19 11:36:45 +0900

    File directory changed

---
+++ Python/Crawling/kospi.py
@@ -0,0 +1,16 @@
+import requests
+import webbrowser
+from bs4 import BeautifulSoup as bs
+
+url = "https://finance.naver.com/"
+url_sise = "https://finance.naver.com/sise/"
+
+response = requests.get(url).text
+soup = bs(response, 'html.parser')
+result = soup.select_one('.num')
+print(result.text)
+
+response_sise = requests.get(url_sise).text
+soup_sise = bs(response_sise, 'html.parser')
+result_sise = soup_sise.select_one('#KOSPI_now')    # class #
+print(f"Kospi : {result_sise.text}")
---
+++ Python/Crawling/lotto.py
@@ -0,0 +1,69 @@
+'''
+import random
+
+numbers = random.sample(range(1,46), 6)
+numbers.sort()
+
+# Add 12.19
+url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=836"
+
+print(numbers)
+
+
+# Add 12. 19
+# requests
+import requests
+
+response = requests.get("https://www.naver.com").text
+print(response)
+'''
+# python init
+import random
+import json
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+numbers = random.sample(range(1,46), 6)
+numbers.sort()
+
+# Add 12.19
+url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=836"
+
+response = requests.get(url)
+#print(response.text)
+
+lotto = json.loads(response.text)
+# print(lotto)
+# print(lotto["drwtNo6"])
+
+print()
+winner = []
+bnusNo = []
+for i in range(1, 7) :
+    winner.append(lotto.get(f"drwtNo{i}"))
+    # winner.append(lotto["drwtNo" + str(i)])
+bnusNo.append(lotto.get("bnusNo"))
+
+print(winner)
+print(bnusNo)
+
+def pickLotto() :
+    picked = sorted(random.sample(range(1, 46), 6))
+    matched = len(set(winner) & set(picked))    # Same num check
+    bnus = len(set(bnusNo) & set(picked))
+
+    if matched == 6 :
+        print("1등")
+    elif matched == 5 and bnus == 1 :       # error?
+        print("2등")
+    elif matched == 5 :
+        print("3등")
+    elif matched == 4 :
+        print("4등")
+    elif matched == 3 :
+        print("5등")
+    else :
+        print("꽝")
+
+print(pickLotto)
